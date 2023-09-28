ba01abf233de43000f79f2bc7f16cc6d0e800d52
Tikkkkkw <Tyc0751@gmail.com>
2023-09-28 07:09:44 -0400

    9/28/2023 Finished the project: - added user input for tipAmount. - completed calculation for the tips needed. - finished the code with the informations. - added rounding to make sure that the money value doesn't have decimal over hundreds place. -tided up the code, removed excess lines, and changed the input from println to print

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args)  {
         Scanner s = new Scanner(System.in);
-        System.out.println("The total amount of people: ");
+        System.out.print("The total amount of people: ");
         int totalPeople = s.nextInt();
-        System.out.println("The bill amount: ");
+        System.out.print("The bill amount: ");
         Double totalBeforeTip = s.nextDouble();
-        System.out.println("Tip percentage: ");
+        System.out.print("Tip percentage: ");
         double tipAmount = s.nextDouble();
         s.close();
 
@@ -17,16 +17,13 @@ public class Main{
         Double tipPerPerson = tipNeeded/totalPeople;
         Double totalPerPerson = totalAmount/totalPeople;
 
-
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Here's your information:");
-        System.out.println("Your Tip amount is: $" + tipNeeded);
-        System.out.println("Your Total Bill is: $" + totalAmount);
-        System.out.println("The amount of Tip Per Person is: $" + tipPerPerson);
-        System.out.println("The Total Bill Per Person is: $" + totalPerPerson);
+        System.out.println("Your Tip amount is: $" + (Math.floor(tipNeeded*100)/100));
+        System.out.println("Your Total Bill is: $" + (Math.floor(totalAmount*100)/100));
+        System.out.println("The amount of Tip Per Person is: $" + (Math.floor(tipPerPerson*100)/100));
+        System.out.println("The Total Bill Per Person is: $" + (Math.floor(totalPerPerson*100)/100));
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-
 
     }
 }
